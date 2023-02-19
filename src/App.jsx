2baa46ca8151f5e6efecfd5b238b95bb1428aca6
Anésio Neto <anesios98@gmail.com>
2023-02-19 17:25:20 -0300

    Bugfix- Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom"
 import Container from "./components/layout/Container"
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import MyData from './components/pages/MyData'
 import Wallet from './components/pages/Wallet'
 
-import Footer from './components/layout/Footer'
-import Navbar from './components/layout/Navbar'
-
 function App() {
   return (
-
     <Router>
-
+      <Navbar />
       <Container customClass="min-height">
-        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/wallet" element={<Wallet />} />
           <Route exact path="/mydata" element={<MyData />} />
         </Routes>
       </Container>
-
       <Footer />
-
     </Router>
   )
 }
